f2a65e3f9358d0ec3496759c9cab454afd03fff6
igobhaktiar <igobhaktiar07@gmail.com>
2024-09-06 23:42:42 +0700

    grid view

--- lib/presentation/widgets/bottom_nav_sheet.dart
+++ lib/presentation/widgets/bottom_nav_sheet.dart
@@ -61,52 +61,62 @@ class BottomNavSheet extends GetWidget<BottomNavSheetController> {
   }
 
   Widget _buildExpandedSheet() {
-    return BottomSheet(
-      onClosing: () {},
-      builder: (BuildContext context) {
-        return SizedBox(
-          height: 500,
-          child: Column(
-            children: [
-              ListTile(
-                leading: const Icon(Icons.home),
-                title: const Text('Beranda'),
-                onTap: () => controller.setSelectedIndex(0),
-              ),
-              ListTile(
-                leading: const Icon(Icons.search),
-                title: const Text('Cari'),
-                onTap: () => controller.setSelectedIndex(1),
-              ),
-              ListTile(
-                leading: const Icon(Icons.shopping_cart),
-                title: const Text('Keranjang'),
-                onTap: () => controller.setSelectedIndex(2),
-              ),
-              ListTile(
-                leading: const Icon(Icons.receipt),
-                title: const Text('Daftar Transaksi'),
-                onTap: () => controller.setSelectedIndex(3),
-              ),
-              ListTile(
-                leading: const Icon(Icons.card_giftcard),
-                title: const Text('Voucher Saya'),
-                onTap: () => controller.setSelectedIndex(4),
-              ),
-              ListTile(
-                leading: const Icon(Icons.location_on),
-                title: const Text('Alamat Pengiriman'),
-                onTap: () => controller.setSelectedIndex(5),
-              ),
-              ListTile(
-                leading: const Icon(Icons.people),
-                title: const Text('Daftar Teman'),
-                onTap: () => controller.setSelectedIndex(6),
-              ),
-            ],
-          ),
-        );
+    final List<Map<String, dynamic>> items = [
+      {
+        'icon': Icons.home,
+        'label': 'Beranda',
+        'onTap': () => controller.setSelectedIndex(0)
       },
+      {
+        'icon': Icons.search,
+        'label': 'Cari',
+        'onTap': () => controller.setSelectedIndex(1)
+      },
+      {
+        'icon': Icons.shopping_cart,
+        'label': 'Keranjang',
+        'onTap': () => controller.setSelectedIndex(2)
+      },
+      {'icon': Icons.receipt, 'label': 'Daftar Transaksi', 'onTap': () {}},
+      {'icon': Icons.card_giftcard, 'label': 'Voucher Saya', 'onTap': () {}},
+      {'icon': Icons.location_on, 'label': 'Alamat Pengiriman', 'onTap': () {}},
+      {'icon': Icons.people, 'label': 'Daftar Teman', 'onTap': () {}},
+      {'icon': Icons.account_circle, 'label': 'Profil', 'onTap': () {}},
+    ];
+
+    return SizedBox(
+      height: 300,
+      child: GridView.builder(
+        padding: const EdgeInsets.all(16),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          childAspectRatio: 1,
+          crossAxisSpacing: 16,
+          mainAxisSpacing: 16,
+        ),
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return _buildGridItem(items[index]);
+        },
+      ),
+    );
+  }
+
+  Widget _buildGridItem(Map<String, dynamic> item) {
+    return InkWell(
+      onTap: item['onTap'],
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(item['icon'], size: 32, color: Colors.blue),
+          const SizedBox(height: 8),
+          Text(
+            item['label'],
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 12),
+          ),
+        ],
+      ),
     );
   }
 }
